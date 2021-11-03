85e90779a76934a9f5d14deaa08e9a808bf84180
cezary turek <cezary.turek@gmail.com>
2021-11-03 21:14:08 +0100

    FDR and FPR

--- dev/test_compare_by_treshold.R
+++ dev/test_compare_by_treshold.R
@@ -39,9 +39,11 @@ comparison_space <- construct_comparison_space()
 # Call method on search space.
 compare_by_treshold <- function(comparison_space, input_gmt_filtered, treshold=0.05) {
   # DEB : i <- 2
-  comparison_res <- data.frame(matrix(ncol = 6, nrow = 0))
+  comparison_res <- data.frame(matrix(ncol = 12, nrow = 0))
   colnames(comparison_res) <- c('pt_PCER', 'bh_PCER', 'pv_PCER', 
-                                'pt_FDR', 'bh_FDR', 'pv_FDR')
+                                'pt_FDR', 'bh_FDR', 'pv_FDR', 
+                                'pt_TPR', 'bh_TPR', 'pv_TPR', 
+                                'pt_FPR', 'bh_FPR', 'pv_FPR')
   for (i in 1:length(comparison_space$no_over_repr_terms)) {
     comparison_case <- comparison_space[i,]
     print(i)
@@ -112,6 +114,34 @@ compare_by_treshold <- function(comparison_space, input_gmt_filtered, treshold=0
     pv_false_positive_no <- length(pv_false_positive) - sum(pv_false_positive)
     pv_FDR <- pv_false_positive_no/length(pv_false_positive)
     
+    # True positive rate (TPR) 
+    # TPR = TP/(TP+FN)
+    pt_ture_positive <- over_repr_in %in% pt_res
+    pt_ture_positive_no <- sum(pt_ture_positive)
+    pt_TPR <- pt_ture_positive_no/length(over_repr_in)
+    
+    bh_ture_positive <- over_repr_in %in% bh_res
+    bh_ture_positive_no <- sum(bh_ture_positive)
+    bh_TPR <- bh_ture_positive_no/length(over_repr_in)
+    
+    pv_ture_positive <- over_repr_in %in% pv_res
+    pv_ture_positive_no <- sum(pv_ture_positive)
+    pv_TPR <- pv_ture_positive_no/length(over_repr_in)
+    
+    # False positive rate (FPR)
+    # FPR = FP/(FP+TN)
+    
+    pt_false_positive <- pt_res %in% over_repr_in
+    pt_false_positive_no <- length(pt_false_positive) - sum(pt_false_positive)
+    pt_FPR <- pt_false_positive_no/(nrow(mulea_ora_results)-length(over_repr_in))
+    
+    bh_false_positive <- bh_res %in% over_repr_in
+    bh_false_positive_no <- length(bh_false_positive) - sum(bh_false_positive)
+    bh_FPR <- bh_false_positive_no/(nrow(mulea_ora_results)-length(over_repr_in))
+    
+    pv_false_positive <- pv_res %in% over_repr_in
+    pv_false_positive_no <- length(pv_false_positive) - sum(pv_false_positive)
+    pv_FPR <- pv_false_positive_no/(nrow(mulea_ora_results)-length(over_repr_in))
     
     # over_repr_pt_res <- over_repr_in %in% pt_res
     # over_repr_bh_res <- over_repr_in %in% bh_res
@@ -123,17 +153,34 @@ compare_by_treshold <- function(comparison_space, input_gmt_filtered, treshold=0
     # bh_res_score_ratio <- bh_res_score / bh_res
     # pv_res_score_ratio <- pv_res_score_ratio / pv_res
     comparison_res[i, ] = data.frame(pt_PCER, bh_PCER, pv_PCER, 
-                                     pt_FDR, bh_FDR, pv_FDR)
+                                     pt_FDR, bh_FDR, pv_FDR, 
+                                     pt_TPR, bh_TPR, pv_TPR,
+                                     pt_FPR, bh_FPR, pv_FPR)
     
   }
   return(comparison_res)
 }
 
 nrow(comparison_space)
-compare_res <- compare_by_treshold(comparison_space = comparison_space[506:540,], 
-                                   input_gmt_filtered = input_gmt_filtered)
 
-boxplot(compare_res)
+comp_spac_small_enr <- comparison_space[1:10,]
+compare_res_small <- compare_by_treshold(comparison_space = comp_spac_small_enr, 
+                                   input_gmt_filtered = input_gmt_filtered)
+boxplot(compare_res_small)
+
+
+comp_spac_small_big <- comparison_space[506:540,]
+compare_res_big <- compare_by_treshold(comparison_space = comp_spac_small_big, 
+                                   input_gmt_filtered = input_gmt_filtered)
+boxplot(compare_res_big)
+plot(x = compare_res_big$bh_FPR, y= compare_res_big$bh_TPR, main="FPR to TPR",
+     xlab="FPR ", ylab="TPR ", xlim = c(0,1), ylim = c(0,1))
+points(x = compare_res_big$pt_FPR,
+       y = compare_res_big$pt_TPR,
+       col = 'red')
+points(x = compare_res_big$pv_FPR,
+       y = compare_res_big$pv_TPR,
+       col = 'blue')
 
 # DELETE - for debug only
 input_gmt=input_gmt_filtered
@@ -143,7 +190,7 @@ under_repr_ratio=0.2
 number_of_over_representation_groups=10 
 number_of_under_representation_groups=10 
 turn_on_log=FALSE
-
+treshold=0.05
 
 
 
