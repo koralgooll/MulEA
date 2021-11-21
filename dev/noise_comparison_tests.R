1f02ba11dd2922e8bc75abaf510b399842228a61
cezary turek <cezary.turek@gmail.com>
2021-11-21 18:53:21 +0100

    ROC curves.

--- dev/noise_comparison_tests.R
+++ dev/noise_comparison_tests.R
@@ -123,16 +123,89 @@ comp_mult_tests %>% ggplot(aes(x=method, y=FPR, fill=method)) +
 # ROC by ratio.
 comp_mult_tests %>% ggplot(aes(FPR, TPR)) + 
   geom_point() +
-  geom_smooth(method='lm', formula= y~x+x^2) +
+  geom_smooth(method='lm', formula= y~x) +
+  facet_wrap(~method) +
   coord_cartesian(xlim=c(0,1), ylim=c(0,1))
 
 comp_mult_tests %>% ggplot(aes(FPR, TPR)) + 
   geom_point() +
   geom_smooth(method='loess', formula= y~x+x^2) +
+  facet_wrap(~method) +
   coord_cartesian(xlim=c(0,1), ylim=c(0,1))
 
 
 
+# ROC on average 3.5 noise_ratio.
+mult_tests_03_04 <- c(mult_tests_03, mult_tests_04)
+
+
+cut_off_range <- seq(0, 1, 0.005)
+
+mult_tests_03_04_sum <- NULL
+for (cut_off in cut_off_range) {
+  mult_tests_03_04_sum_p <- MulEA:::getMultipleTestsSummary(
+    tests_res = mult_tests_03_04, comparison_col_name = 'pValue', 
+    labels = list('method'='p', 'cut_off'=cut_off), cut_off = cut_off)
+  
+  mult_tests_03_04_sum_bh <- MulEA:::getMultipleTestsSummary(
+    tests_res = mult_tests_03_04, comparison_col_name = 'adjustedPValue', 
+    labels = list('method'='bh', 'cut_off'=cut_off), cut_off = cut_off)
+  
+  mult_tests_03_04_sum_pt <- MulEA:::getMultipleTestsSummary(
+    tests_res = mult_tests_03_04, comparison_col_name = 'adjustedPValueEmpirical', 
+    labels = list('method'='pt', 'cut_off'=cut_off), cut_off = cut_off)
+  
+  mult_tests_03_04_sum <- rbind(mult_tests_03_04_sum, mult_tests_03_04_sum_p, 
+                                mult_tests_03_04_sum_pt, mult_tests_03_04_sum_bh)
+}
+
+
+# ROC curve plot.
+mult_tests_03_04_sum_grouped <- mult_tests_03_04_sum %>% 
+  group_by(method, cut_off) %>% 
+  summarize(TPR_mean = mean(as.numeric(TPR)), FPR_mean = mean(as.numeric(FPR)))
+
+mult_tests_03_04_sum_grouped %>% ggplot(aes(FPR_mean, TPR_mean, colour=method)) + 
+  geom_point() +
+  geom_step() +
+  facet_wrap(~method) +
+  geom_abline(color="black") +
+  scale_fill_brewer(palette="PuBu")
+
+mult_tests_03_04_sum_grouped %>% ggplot(aes(FPR_mean, TPR_mean, colour=method)) + 
+  geom_point() +
+  geom_step() +
+  geom_abline(color="black") +
+  scale_fill_brewer(palette="PuBu")
+
+
+# TODO IMPORTANT : Analyze it!!
+# IDEA : More enrichment points will produce better estimates of densities.
+# IDEA : Use all ratios, not only 0.3 and 0.4.
+mult_tests_03_04_sum %>% ggplot(aes(FPR, colour=method)) + 
+  stat_ecdf() + 
+  geom_abline(color="black")
+
+mult_tests_03_04_sum %>% ggplot(aes(FPR, fill = method, colour = method)) + 
+  geom_density(adjust = 4, alpha = 0.1)
+
+
+
+mult_tests_03_04_sum %>% ggplot(aes(TPR, colour=method)) + 
+  stat_ecdf() + 
+  geom_abline(color="black")
+
+mult_tests_03_04_sum %>% ggplot(aes(TPR, fill = method, colour = method)) + 
+  geom_density(adjust = 1, alpha = 0.1)
+
+mult_tests_03_04_sum %>% ggplot(aes(TPR, fill = method, colour = method)) + 
+  geom_density(adjust = 7, alpha = 0.1)
+
+
+
+
+
+# TODO : Move to Utils.
 simulateMultipleTests <- function(
   input_gmt_filtered, number_of_tests = 10, 
   noise_ratio = 0.2,
