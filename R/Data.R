fc6402a8aee5db40b9305b7678bd714fab9b4509
Tamas Stirling <stirling.tamas@gmail.com>
2022-06-12 20:17:59 +0200

    Store external data as rda files in 'data' directory

---
+++ R/Data.R
@@ -0,0 +1,39 @@
+#' Gene Set Example in GMT Format
+#'
+#' A data frame containing 7 gene sets in Gene Matrix Transposed (GMT) format.
+#' Each row represents a gene set. 
+#'
+#' @format A data frame with 7 rows and 3 variables:
+#' \describe{
+#'   \item{ontologyId}{Ontology ID of the gene set}
+#'   \item{ontologyName}{Description of the gene set}
+#'   \item{listOfValues}{Genes in the gene set}
+#' }
+#' @source \url{http://geneontology.org/}
+"geneSet"
+
+#' TITLE
+#'
+#' DESCRIPTION
+#'
+#' @format A data frame with 13 rows and 3 variables:
+#' \describe{
+#'   \item{X}{Row names}
+#'   \item{select}{DEFINITION}
+#'   \item{score}{DEFINITION}
+#' }
+#' @source SOURCE
+"selectDf"
+
+#' TITLE
+#'
+#' DESCRIPTION
+#'
+#' @format A data frame with 33 rows and 2 variables:
+#' \describe{
+#'   \item{X}{Row names}
+#'   \item{pool}{DEFINITION}
+#' }
+#' @source SOURCE
+"poolData"
+
