f8e979401101a75c104d900894ae036320ce49b7
cezary turek <cezary.turek@gmail.com>
2022-06-22 14:38:08 +0200

    Solve problem with mutlicore tests.

--- tests/testthat/testHypergeometricTest.R
+++ tests/testthat/testHypergeometricTest.R
@@ -12,7 +12,7 @@ test_that("ORA : object creation test.", {
   
   mulea_ora_model <- MulEA::ORA(
     gmt = gmtMock, testData = testDataMock, pool=poolMock,
-    adjustMethod = "PT")
+    adjustMethod = "PT", nthreads = 2)
   
   testthat::expect_equal(mulea_ora_model@gmt, gmtMock)
   testthat::expect_equal(mulea_ora_model@testData, c("a", "b", "c"))
@@ -26,7 +26,8 @@ test_that("ORA : testData out of DB model.", {
                         stringsAsFactors = FALSE)
   testDataMock <- c("a", "b", "d")
 
-  mulea_ora_model <- MulEA::ORA(gmt = gmtMock, testData = testDataMock, adjustMethod = "PT")
+  mulea_ora_model <- MulEA::ORA(gmt = gmtMock, testData = testDataMock, 
+                                adjustMethod = "PT", nthreads = 2)
   
   testthat::expect_warning(muleaTestRes <- MulEA::runTest(mulea_ora_model))
   testthat::expect_equal(muleaTestRes$pValue, 1)
@@ -42,7 +43,7 @@ test_that("ORA : testData out of pool.", {
 
   mulea_ora_model <- MulEA::ORA(
     gmt = gmtMock, testData = testDataMock, pool=poolMock,
-    adjustMethod = "PT")
+    adjustMethod = "PT", nthreads = 2)
   
   testthat::expect_warning(muleaTestRes <- MulEA::runTest(mulea_ora_model))
   testthat::expect_equal(muleaTestRes$pValue, 1/3)
@@ -58,7 +59,7 @@ test_that("ORA : matrix 2,2,2,2.", {
 
   mulea_ora_model <- MulEA::ORA(
     gmt = gmtMock, testData = testDataMock, pool=poolMock,
-    adjustMethod = "PT")
+    adjustMethod = "PT", nthreads = 2)
   
   muleaTestRes <- MulEA::runTest(mulea_ora_model)
   testthat::expect_equal(muleaTestRes$pValue, 53/70)
@@ -75,7 +76,7 @@ test_that("ORA : pool >> var + DBi, matrix 2,2,2,18.", {
 
   mulea_ora_model <- MulEA::ORA(
     gmt = gmtMock, testData = testDataMock, pool=poolMock,
-    adjustMethod = "PT")
+    adjustMethod = "PT", nthreads = 2)
   
   muleaTestRes <- MulEA::runTest(mulea_ora_model)
   testthat::expect_equal(muleaTestRes$pValue, 37/322)
@@ -91,7 +92,7 @@ test_that("ORA : DBi not include pool, matrix 2,0,2,2.", {
 
   mulea_ora_model <- MulEA::ORA(
     gmt = gmtMock, testData = testDataMock, pool=poolMock,
-    adjustMethod = "PT")
+    adjustMethod = "PT", nthreads = 2)
   
   muleaTestRes <- MulEA::runTest(mulea_ora_model)
   testthat::expect_equal(muleaTestRes$pValue, 0.4)
@@ -110,7 +111,7 @@ test_that("ORA : DB1 + DB2 => pool, matrix 1,3,2,2 and 2,2,1,3.", {
   testDataMock <- c("d", "e", "f")
 
   mulea_ora_model <- MulEA::ORA(gmt = gmtMock, 
-    testData = testDataMock, adjustMethod = "PT")
+    testData = testDataMock, adjustMethod = "PT", nthreads = 2)
   
   muleaTestRes <- MulEA::runTest(mulea_ora_model)
   testthat::expect_equal(muleaTestRes$pValue, c(13/14, 0.5))
@@ -132,8 +133,25 @@ test_that("ORA : DB1 + DB2 => pool, matrix 2,2,2,0 and 2,2,1,3.", {
 
   mulea_ora_model <- MulEA::ORA(
     gmt = gmtMock, testData = testDataMock, pool=poolMock,
-    adjustMethod = "PT")
+    adjustMethod = "PT", nthreads = 2)
   
   muleaTestRes <- MulEA::runTest(mulea_ora_model)
   testthat::expect_equal(muleaTestRes$pValue, c(37/322, 27/3542))
 })
+
+
+test_that("ORA : DB1 + DB2 => pool, matrix 2,2,2,0 and 2,2,1,3.", {
+  gmtMock <- MulEA::readGmtFileAsDataFrame(gmtFilePath = system.file(package="MulEA", "extdata", "model.gmt"))
+  testDataMock <- c("FBgn0004407", "FBgn0010438", "FBgn0037044", "FBgn0002887", "FBgn0028434", "FBgn0030170", "FBgn0263831")
+  poolMock <- unique(c(c("FBgn0033690", "FBgn0261618", "FBgn0004407", "FBgn0010438", "FBgn0032154", "FBgn0039930", "FBgn0040268", "FBgn0013674",
+                         "FBgn0037008", "FBgn0003116", "FBgn0037743", "FBgn0035401", "FBgn0037044", "FBgn0051005", "FBgn0026737", "FBgn0026751",
+                         "FBgn0038704", "FBgn0002887", "FBgn0028434", "FBgn0030170", "FBgn0263831", "FBgn0000579"),
+                       c("FBgn0066666", "FBgn0000000", "FBgn0099999", "FBgn0011111", "FBgn0022222", "FBgn0777777", "FBgn0333333")))
+  
+  mulea_ora_model <- MulEA::ORA(
+    gmt = gmtMock, testData = testDataMock, pool=poolMock, nthreads = 2)
+  
+  muleaTestRes <- MulEA::runTest(mulea_ora_model)
+  testthat::expect_equal(muleaTestRes$pValue[c(1,2)], c(1, 19205/26423))
+})
+
