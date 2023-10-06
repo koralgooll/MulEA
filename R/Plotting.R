43ac1f6d317b86242ef3b053af440d466ebbf6bb
olbeimarton <olbei.marton@gmail.com>
2023-10-06 16:22:47 +0100

    Update Plotting.R

--- R/Plotting.R
+++ R/Plotting.R
@@ -253,7 +253,7 @@ plot_graph <- function(reshaped_results,
     scale_color_gradient2(
       mid = '#004687',
       high = '#ffa600',
-      limits = c(0.0, paste0(as.numeric(p_value_max_threshold))) +
+      limits = c(0.0, paste0(as.numeric(p_value_max_threshold))),
       name = p_value_type_colname
     ) + 
     ggraph::geom_node_text(aes(label = .data$label), repel = TRUE) +
