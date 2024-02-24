1aa0164fcc1f1f2881222a267e967dd790bc14fb
barizona <barizona@barizona>
2024-02-24 13:22:16 +0100

    Descriptions of Data.R was changed. Roxygen: list(markdown = TRUE) was added to DEACRIPTION

--- R/Data.R
+++ R/Data.R
@@ -1,4 +1,4 @@
-#' Gene Set Example
+#' Ontology Example
 #'
 #' Data frame parsed from a Gene Matrix Transposed (GMT) file containing 7 gene
 #' sets. Each row represents a gene set. 
@@ -12,28 +12,29 @@
 #' @source \url{http://geneontology.org/}
 "geneSet"
 
-#' TITLE
+#' Genes and scores for ora and gsea analyses
 #'
-#' DESCRIPTION
+#' Data frame containing 13 genes in the "select" column and their scores in the
+#' "score" column.
 #'
 #' @format A data frame with 13 rows and 3 variables:
 #' \describe{
 #'   \item{X}{Row names}
-#'   \item{select}{DEFINITION}
-#'   \item{score}{DEFINITION}
+#'   \item{select}{Flybase gene IDs of 13 genes from Drosophyla melanogaster}
+#'   \item{score}{Log fold change scores for each gene}
 #' }
-#' @source SOURCE
+#' @source \url{https://flybase.org/}
 "selectDf"
 
-#' TITLE
+#' Background genes for ora analyses
 #'
-#' DESCRIPTION
+#' Data frame containing 13 genes in the "background_element_names" column.
 #'
 #' @format A data frame with 33 rows and 2 variables:
 #' \describe{
 #'   \item{X}{Row names}
-#'   \item{background_element_names}{DEFINITION}
+#'   \item{background_element_names}{Flybase gene IDs of 33 genes from Drosophyla melanogaster}
 #' }
-#' @source SOURCE
+#' @source \url{https://flybase.org/}
 "poolDf"
 
